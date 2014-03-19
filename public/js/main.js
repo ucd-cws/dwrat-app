88510ed0ba2a40f2fffc7875597c8e551b526e8c
Justin Merz <jrmerz@gmail.com>
2014-03-19 16:52:03 -0700

    changing colors, size for demand, can now see multiple at same location

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,8 @@ var markerTemplate =
 	'<h3>Water Right #: {{application_number}}</h3>'+
 	'<div style="margin:15px">'+
 		'<b>For:</b> {{date}}<br />'+
-		'<b>Allocation:</b> {{allocation}} [Ac-ft/day]'+
+		'<b>Allocation:</b> {{allocation}} [Ac-ft/day]<br />'+
+		'<b>Demand:</b> {{demand}} [Ac-ft/day]'+
 		'<div class="well well-sm" style="margin-top:5px">'+
 		'{{water_right_type}}<br />'+
 		'<b>Priority Date:</b> {{priority_date}}'+
@@ -77,6 +78,17 @@ function init() {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
+	L.tileLayer.wms("https://mapsengine.google.com/15542600734275549444-11853667273131550346-4/wms/?version=1.3.0", {
+	    layers: '15542600734275549444-08112974690991164587-4',
+	    format: 'image/png',
+	    transparent: true,
+	    attribution: "",
+	    bounds : L.latLngBounds(
+	    	L.latLng(42.00943198804058, -114.1307597213407),
+	    	L.latLng(32.53426345367493, -124.40971171678298)
+	    )
+	}).addTo(map);
+
 
 	$('#close-popup').on('click', function(){
 		$('#info').removeClass('fadeInDown').addClass('slideOutRight');
@@ -86,6 +98,29 @@ function init() {
 		}, 800);
 		
 	});
+
+	getWatersheds();
+}
+
+function getWatersheds() {
+	var query = new google.visualization.Query('http://watershed.ice.ucdavis.edu/vizsource/rest?view=watersheds&tq=SELECT * order by watershed');
+  	query.send(function(response){
+  		if (response.isError()) {
+	      $('#response').html('<div class="alert alert-danger">Error in query: ' + response.getMessage() + ' ' 
+	      		+ response.getDetailedMessage()+'</div>');
+	      return;
+	    }
+
+	    var data = response.getDataTable();
+	    var select = $('#watershed');
+	    for( var i = 0; i < data.getNumberOfRows(); i++ ) {
+    		if( data.getValue(i, 0) ) select.append('<option value="'+data.getValue(i, 0)+'" '+
+    			(data.getValue(i, 0) == 'EEL RIVER' ? 'selected' : '') + ' >'+
+    			data.getValue(i, 0)+'</option>');
+    	};
+
+    	$('#exec').removeClass('disabled').html('Execute');
+  	});
 }
 
 function resize() {
@@ -140,8 +175,6 @@ function onDataLoad(response) {
 	    }
     };
 
-    console.log(points.length);
-
     var l = L.geoJson(points,{
 		pointToLayer: function (feature, latlng) {
 			var type = feature.properties.riparian ? 'riparian' : 'application';
@@ -151,11 +184,33 @@ function onDataLoad(response) {
 				if( feature.properties.allocations[i].allocation > 0 ) allocation = 'allocated';
 			}
 
+			
+			var options = $.extend(true, {}, markers[type][allocation]);
+
+			if( feature.properties.allocations && feature.properties.allocations.length > 0 ) {
+				var demand = feature.properties.allocations[0].demand;
+				var allocation = feature.properties.allocations[0].allocation;
+
+				
+				if( demand == 0 ) {
+					options.fillColor = '#333';
+					options.radius = 6;
+					options.fillOpacity = .7;
+				} else {
+					var size = Math.sqrt(allocation) * 3;
+					//if( size > 25 ) size = 25;
+					if( size < 7 ) size = 7;
+					options.radius = size;
+				}	
+			}
+
+
+
 			var marker;
 			if( feature.properties.riparian ) {
-				marker = L.polyMarker(latlng, markers[type][allocation]);
+				marker = L.polyMarker(latlng, options);
 			} else {
-				marker = L.circleMarker(latlng, markers[type][allocation]);
+				marker = L.circleMarker(latlng, options);
 			}
 
 			allData.push({
@@ -180,14 +235,25 @@ function onDataLoad(response) {
 			}
 		}*/
 
+		var ll = e.layer.feature.geometry.coordinates;
+		console.log(e.layer.feature);
+		var t, html = '';
+		for( var i = 0; i < allData.length; i++ ) {
+			t = allData[i].feature.geometry.coordinates;
+			if( ll[0].toFixed(4) == t[0].toFixed(4) && ll[1].toFixed(4) == t[1].toFixed(4) ) {
+				html += stamp({
+					application_number : allData[i].feature.properties.application_number,
+					date : '2014-03-01',
+					allocation : allData[i].feature.properties.allocations[0].allocation,
+					demand : allData[i].feature.properties.allocations[0].demand,
+					water_right_type : allData[i].feature.properties.water_right_type,
+					priority_date : allData[i].feature.properties.priority_date
+				});
+			}
+		}
+
 		
-		$('#info-result').html(stamp({
-			application_number : props.application_number,
-			date : '2014-03-01',
-			allocation : props.allocations[0].allocation,
-			water_right_type : props.water_right_type,
-			priority_date : props.priority_date
-		}));
+		$('#info-result').html(html);
 	});
 }
 
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ app.use(express.static('public'));
 
 
 
-app.listen(3000);
-console.log('running on localhost:3000/test.html');
+app.listen(3001);
+console.log('running on localhost:3001');

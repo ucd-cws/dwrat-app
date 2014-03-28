97c08378180cc36e53363bba70846efcbeb81374
Justin Merz <jrmerz@gmail.com>
2014-03-28 10:21:29 -0700

    adding huc12 layer, layer control, and new query table

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 var map, geoJsonLayer;
 
+var wmsLayers = [];
 
 $(window).on('ready', function(){
 	resize();
@@ -79,7 +80,7 @@ function init() {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-	L.tileLayer.wms("https://mapsengine.google.com/15542600734275549444-11853667273131550346-4/wms/?version=1.3.0", {
+	var wms1 = L.tileLayer.wms("https://mapsengine.google.com/15542600734275549444-11853667273131550346-4/wms/?version=1.3.0", {
 	    layers: '15542600734275549444-08112974690991164587-4',
 	    format: 'image/png',
 	    transparent: true,
@@ -89,6 +90,24 @@ function init() {
 	    	L.latLng(32.53426345367493, -124.40971171678298)
 	    )
 	}).addTo(map);
+
+	var wms2 = L.tileLayer.wms("https://mapsengine.google.com/15542600734275549444-11853667273131550346-4/wms/?version=1.3.0", {
+	    layers: '15542600734275549444-16143158689603361093-4',
+	    format: 'image/png',
+	    transparent: true,
+	    attribution: "",
+	    bounds : L.latLngBounds(
+	    	L.latLng(42.00943198804058, -114.1307597213407),
+	    	L.latLng(32.53426345367493, -124.40971171678298)
+	    )
+	});
+
+	var baseMaps = {};
+	var overlayMaps = {
+		'Watersheds': wms1,
+		'HUC 12': wms2
+	};
+	L.control.layers(baseMaps, overlayMaps, {position: 'bottomleft'}).addTo(map);
 
 
 	$('#close-popup').on('click', function(){
